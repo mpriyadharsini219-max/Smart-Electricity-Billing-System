b6f1baa5d2e2baeb8f12514d55acb01d0496c96d
PRIYA DHARSINI M <mpriyadharsini219@gmail.com>
2026-05-17 22:19:53 +0530

    Create admin.java

---
+++ admin.java
@@ -0,0 +1,9 @@
+public class Admin {
+
+    public void showDashboard() {
+        System.out.println("Admin Dashboard");
+        System.out.println("1. View Consumers");
+        System.out.println("2. Generate Bills");
+        System.out.println("3. Payment Status");
+    }
+}
